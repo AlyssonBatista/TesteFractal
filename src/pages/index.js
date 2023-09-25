bc1f5b3ddb5993a50a4d3664dac49f6583cb513d
Alysson Batista de Souza <alyssonbatista241@gmail.com>
2023-09-25 16:55:34 -0300

    config header

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,17 @@ import Footer from '../components/footer/footer.jsx'
 import Main from "../components/main/main.jsx"
 import { Link } from 'gatsby'
 import * as index from "../pages/index.module.css"
-
+ 
 
 export default function Por() {
   return (
     <>
       <Head />
-      <header className={index.headerGrid}>
+       <header className={index.headerGrid}>
         <p>Idiomas:</p>
         <Link className={index.ativo} to="/">PT</Link>
         <Link className={index.linkInativo} to="/index2">ES</Link>
-      </header>
+      </header> 
       <Main />
       <Footer />
     </>
