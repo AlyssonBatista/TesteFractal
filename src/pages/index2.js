a2e6bef74d3a67048d594995d14a84266c5bf187
Alysson Batista de Souza <alyssonbatista241@gmail.com>
2023-09-26 14:22:26 -0300

    correção section 4

--- src/pages/index2.js
+++ src/pages/index2.js
@@ -1,34 +1,29 @@
-import * as React from "react"
-import Head from "../components/head/head.jsx"
-import Footer from '../components/footer/footer.jsx'
+import * as React from "react";
+import Head from "../components/head/head.jsx";
+import Footer from '../components/footer/footer.jsx';
 import Section1 from "../components/section1/section1";
 import Section2 from "../components/section2/section2";
 import Section3 from "../components/section3/section3";
 import Section4 from "../components/section4/section4";
-import { Link } from 'gatsby'
-import * as index2 from "../pages/index2.module.css"
-import { graphql } from "gatsby"
+import * as index from "../pages/index.module.css";
+import { Link } from 'gatsby';
+import { graphql } from "gatsby";
 
 export default function Esp({ data }) {
-  const { descricao,titulo,li1,span1,li2,span2,li3,span3,li4,span4,titulo2,descricao2,titulo3,descricao3,botaoInscricao,regulamento,titulo4,botaoInscricao2,footerRea,footerApo  } = data.site.siteMetadata.espanhol
+  const { descricao, titulo, li1, span1, li2, span2, li3, span3, li4, span4, titulo2, descricao2, titulo3, descricao3, botaoInscricao, regulamento, titulo4, botaoInscricao2, footerRea, footerApo } = data.site.siteMetadata.espanhol
   return (
     <>
       <Head />
-      <header className={index2.headerGrid}>
+      <header className={index.headerGrid}>
         <p>Idiomas:</p>
-        <Link className={index2.linkInativo} to="/">PT</Link>
-        <Link className={index2.ativo} to="/index2">ES</Link>
+        <Link className={index.inativo} to="/">PT</Link>
+        <Link className={index.ativo} to="/index2">ES</Link>
       </header>
-
       <Section1 p={descricao} h2={titulo} li1={li1} span1={span1} li2={li2} span2={span2} li3={li3} span3={span3} li4={li4} span4={span4} />
-
-      <Section2 h1={titulo2} p={descricao2}/>
-
-      <Section3 h1={titulo3} p={descricao3} span={botaoInscricao} a={regulamento}/>
-
-      <Section4 h1={titulo4} a={botaoInscricao2}/>
-
-      <Footer  p1={footerRea} p2={footerApo} />
+      <Section2 h1={titulo2} p={descricao2} />
+      <Section3 h1={titulo3} p={descricao3} span={botaoInscricao} a={regulamento} />
+      <Section4 h1={titulo4} a={botaoInscricao2} />
+      <Footer p1={footerRea} p2={footerApo} />
     </>
   )
 }
